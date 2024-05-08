e9b4af6bbea8849df8a988b56bef6cec662b0fd3
m-ouaali <m.ouaali@student.avans.nl>
2024-05-08 13:20:20 +0200

    testen met azure2

--- examples/mysql/mysql-example-pool.js
+++ examples/mysql/mysql-example-pool.js
@@ -40,47 +40,4 @@ pool.on('acquire', function (connection) {
     logger.trace('Connection %d acquired', connection.threadId)
 })
 
-pool.on('release', function (connection) {
-    logger.trace('Connection %d released', connection.threadId)
-})
-
-// Hier wordt de query gedefinieerd met behulp van ? placeholders (prepared statement)
-queryString = 'SELECT * FROM `user` WHERE `firstName` = ? AND `id` > ?'
-name = 'Herman'
-isActive = 1
-
-// use the pool to get one connection from the pool
-pool.getConnection(function (err, connection) {
-    if (err) {
-        logger.error(err)
-        // in je server: next(err)!
-        return 1 // exit the program, alleen hier omdat het een voorbeeld is
-    }
-
-    // Use the connection to execute a query
-    connection.query(
-        queryString,
-        [name, isActive],
-        function (error, results, fields) {
-            // When done with the connection, release it.
-            connection.release()
-
-            // Handle error after the release.
-            if (err) {
-                logger.error(err)
-                // in je server: next(err)!
-                return 1 // exit the program, alleen hier omdat het een voorbeeld is
-            }
-
-            // Don't use the connection here, it has been returned to the pool.
-            logger.debug('#results = ', results.length)
-            logger.debug({
-                statusCode: 200,
-                results: results
-            })
-        }
-    )
-})
-
-// Export the pool, so that testcases can use it
-module.exports = pool
+//
